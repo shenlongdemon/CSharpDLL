4e3e893c112012546e053e58025f2f088d7c7689
shenlongdemon <shenlongdemon@gmail.com>
2015-10-27 10:26:44 +0700

    Add WebApiUtil

---
+++ WebApiUtility/WebApiUtility/WebApiUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiUtility
+{
+    public class WebApiUtil
+    {
+        public static async Task<HttpResponseMessage> GetAsync(string url)
+        {           
+            HttpClient client = new HttpClient();
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/*"));
+
+            var response = await client.GetAsync(url);
+            return response;
+        }
+    }
+}
