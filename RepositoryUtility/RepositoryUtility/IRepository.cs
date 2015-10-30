baf3448bf7c646f198aff7ce3807bfbaae055c8b
shenlongdemon <shenlongdemon@gmail.com>
2015-10-31 01:26:49 +0700

    add RepositoryUtility project

---
+++ RepositoryUtility/RepositoryUtility/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryUtility
+{
+    public interface IRepository<TEntity>: IDisposable where TEntity : class
+    {
+        TEntity First(Expression<Func<TEntity, bool>> predicate);
+        TEntity Insert(TEntity entity);
+        Task<TEntity> InsertAsync(TEntity entity);
+        TEntity Update(TEntity entity);
+        int Delete(TEntity entity);
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
